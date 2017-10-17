fc11a4b6eb355f2729f99b2ac127736830ebae42
hmna <htut.aung@mail.utoronto.ca>
2017-10-17 03:41:08 -0400

    fixed local to actual UTC time for clocks and cleaned up js functions

--- js/clocks.js
+++ js/clocks.js
@@ -1,37 +1,38 @@
-const secondHand = document.querySelector('.second-hand');
-const minsHand = document.querySelector('.min-hand');
-const hourHand = document.querySelector('.hour-hand');
+var secondHand = document.querySelectorAll('.second-hand');
+var minsHand = document.querySelectorAll('.min-hand');
+var hourHand = document.querySelectorAll('.hour-hand');
 
-const secondHand2 = document.querySelector('.second-hand2');
-const minsHand2 = document.querySelector('.min-hand2');
-const hourHand2 = document.querySelector('.hour-hand2');
+function transformHand(city, offset) {
 
-const secondHand3 = document.querySelector('.second-hand3');
-const minsHand3 = document.querySelector('.min-hand3');
-const hourHand3 = document.querySelector('.hour-hand3');
+  var seconds = city.getSeconds();
+  var secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand[offset].style.transform = `rotate(${secondsDegrees}deg)`;
+
+  var mins = city.getMinutes();
+  var minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+  minsHand[offset].style.transform = `rotate(${minsDegrees}deg)`;
+
+  var hour = city.getHours();
+  var hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
+  hourHand[offset].style.transform = `rotate(${hourDegrees}deg)`;
+}
 
 function setDate() {
+  
+  //current location date
   const now = new Date();
 
-  const seconds = now.getSeconds();
-  const secondsDegrees = ((seconds / 60) * 360) + 90;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-  secondHand2.style.transform = `rotate(${secondsDegrees}deg)`;
-  secondHand3.style.transform = `rotate(${secondsDegrees}deg)`;
+  //convert to UTC milliseconds
+  var utc = now.getTime() + (now.getTimezoneOffset() * 60000); 
 
-  const mins = now.getMinutes();
-  const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
-  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
-  minsHand2.style.transform = `rotate(${minsDegrees}deg)`;
-  minsHand3.style.transform = `rotate(${minsDegrees}deg)`;
+  //get timezones
+  var toronto = new Date(utc + (3600000* -4));
+  var tokyo = new Date(utc + (3600000* 9));
+  var singapore = new Date(utc + (3600000* 8));
 
-  const hour = now.getHours();
-  const hour2 = now.getHours() +1;
-  const hourDegrees = ((hour / 12) * 360) + ((mins/60)*30) + 90;
-  const hourDegrees2 = ((hour2 / 12) * 360) + ((mins/60)*30) + 90;
-  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-  hourHand2.style.transform = `rotate(${hourDegrees2}deg)`;
-  hourHand3.style.transform = `rotate(${hourDegrees}deg)`;
+  transformHand(toronto, 0);
+  transformHand(tokyo, 1);
+  transformHand(singapore, 2);
 }
 
 setInterval(setDate, 1000);
